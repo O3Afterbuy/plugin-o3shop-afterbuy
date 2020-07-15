81209d6bdbf8cef3f7581e7a56a7d7e6f2eeed14
mdoellerer-fc <marcos.doellerer@fatchip.de>
2020-07-15 12:22:10 +0200

    0064020 - Both branches applied here and conflicts solved

--- copy_this/modules/fc/fcoxid2afterbuy/core/fco2aartimport.php
+++ copy_this/modules/fc/fcoxid2afterbuy/core/fco2aartimport.php
@@ -5,7 +5,7 @@
  * Date: 20.11.18
  */
 
-class fco2aartimport extends fco2abase
+class fco2aartimport extends fco2abaseimport
 {
     private const AFTERBUY_BASE_PRODUCT_FLAG_PARENT = 1;
     private const AFTERBUY_BASE_PRODUCT_FLAG_CHILD = 3;
@@ -280,24 +280,6 @@ class fco2aartimport extends fco2abase
         $oArticlePrice->assign($aParams);
     }
 
-    /**
-     * Adds identification data to oxid product
-     *
-     * @param object $oXmlProduct
-     * @param object $oArticle
-     */
-    protected function _fcAddProductAmounts($oXmlProduct, &$oArticle)
-    {
-        $oArticle->oxarticles__oxstock =
-            new oxField((int) $oXmlProduct->Quantity);
-        $oArticle->oxarticles__oxunitname =
-            new oxField((string) $oXmlProduct->UnitOfQuantity);
-        $oArticle->oxarticles__oxunitquantity =
-            new oxField((int) $oXmlProduct->BasepriceFactor);
-        $oArticle->oxarticles__oxweight =
-            new oxField((int) $oXmlProduct->Weight);
-
-    }
 
     /**
      * Adds identification data to oxid product
@@ -318,55 +300,6 @@ class fco2aartimport extends fco2abase
             $sParentId = $this->_fcFetchParentId($oXmlProduct);
             $oArticle->oxarticles__oxparentid = new oxField($sParentId);
         }
-    }
-
-    /**
-     * declare articles as invalid if article number does not match the configurated conditions
-     *
-     * @param $oXmlProduct
-     * @return bool
-     */
-    protected function _fcCheckIfArticleNumberIsValid($oXmlProduct) {
-        $blDiscard = $this->getConfig()->getConfigParam('blFcAfterbuyIgnoreArticlesWithoutNr');
-
-        if($blDiscard != true) {
-            return true;
-        }
-
-        $sArtNum = $this->_fcGetArticleNumber($oXmlProduct);
-
-        if(empty($sArtNum) || $sArtNum == 0) {
-            $this->oDefaultLogger->fcWriteLog(
-                "INFO: Product has been discarded because of missing article number \n".
-                print_r($oXmlProduct ,true), 2);
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Assign article number based on given config
-     *
-     * @param $oXmlProduct
-     * @return string
-     */
-    protected function _fcGetArticleNumber($oXmlProduct) {
-        $sSource = $this->getConfig()->getConfigParam('sFcAfterbuyImportArticleNumber');
-
-        switch($sSource) {
-            case '0': $sArtNum = $oXmlProduct->EAN ?: $oXmlProduct->Anr;
-                break;
-            case '1': $sArtNum = $oXmlProduct->EAN;
-                break;
-            case '2': $sArtNum = $oXmlProduct->ProductID;
-                break;
-            case '3': $sArtNum = $oXmlProduct->Anr;
-                break;
-            default: $sArtNum = $oXmlProduct->EAN ?: $oXmlProduct->Anr;
-        }
-
-        return $sArtNum;
     }
 
     /**
@@ -765,49 +698,4 @@ class fco2aartimport extends fco2abase
         fclose($oFile);
     }
 
-    /**
-     * Returns next page. If no next page available return zero
-     *
-     * @param $oXmlResponse
-     * @return int
-     */
-    protected function _fcGetNextPage($oXmlResponse)
-    {
-        $iAvailablePages =
-            (int) $oXmlResponse->Result->PaginationResult->TotalNumberOfPages;
-
-        $iCurrentPage =
-            (int) $oXmlResponse->Result->PaginationResult->PageNumber;
-
-        $iNextPage =
-            ($iCurrentPage<$iAvailablePages) ? ++$iCurrentPage : 0;
-
-        return $iNextPage;
-    }
-
-
-    /**
-     * Returns oxid of product if exists or false if not
-     *
-     * @param $oXmlProduct
-     * @return mixed
-     */
-    protected function _fcProductExists($oXmlProduct)
-    {
-        $sProductId = (int) $oXmlProduct->ProductID;
-
-        $oDb = oxDb::getDb();
-
-        $sQuery = "
-            SELECT 
-                OXID 
-            FROM 
-                oxarticles_afterbuy
-            WHERE 
-                fcafterbuyid = ".$oDb->quote($sProductId);
-
-        $mOxid = $oDb->getOne($sQuery);
-
-        return $mOxid;
-    }
 }
